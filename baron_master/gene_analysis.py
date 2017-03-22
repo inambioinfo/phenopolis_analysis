963148aaa144ff5e738af66a26f34d488978c5d3
Nikolas Pontikos <npontikos@gmail.com>
2017-03-22 16:17:10 +0000

    added pLI

--- baron_master/gene_analysis.py
+++ baron_master/gene_analysis.py
@@ -3,10 +3,14 @@ import pymongo
 
 conn = pymongo.MongoClient('phenotips.cs.ucl.ac.uk')
 db=conn['uclex']
+exac=conn['exac']
 
-print('gene_name','gene_length','variants_num',sep=',')
+print('gene_name','gene_length','variants_num','pli',sep=',')
 for gene in db.genes.find():
     gene_length=gene['stop']-gene['start']
     variants_num=len([v for v in db.variants.find({'canonical_gene_name_upper':gene['gene_name']})])
     if variants_num == 0: continue
-    print(gene['gene_name'],gene_length,variants_num,sep=',')
+    g=exac.pli.find_one({'gene':gene['gene_name']})
+    if g: pli=g.get('pLI','NA')
+    else: pli='NA'
+    print(gene['gene_name'],gene_length,variants_num,pli,sep=',')
